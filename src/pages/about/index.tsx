bd5446ca552d99db20facd867c6fa1fadc5334a7
jihye <ghe.lee19@gmail.com>
2023-03-27 13:18:25 +0900

    feat(contents): About 소개글 수정

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -9,10 +9,10 @@ import resumeData from '../../data/resume.json'
 
 export const frontmatter = {
     title: `안녕하세요, 이지혜입니다`,
-    subtitle: ``,
-    description: `2011년, 직군이 생소하던 그때부터 프론트엔드 웹 개발을 하고 있습니다. 웹 표준에 맞춰 의미 있는 구조를 가지며, 디자인과 UX를 다양한 환경에 반응하는, (검색엔진을 포함하여) 모두가 접근 가능한 웹사이트를 만들기 위해 노력하고 있습니다.`,
+    subtitle: `👩‍🎨 👩‍💻 🚀 📈`,
+    description: `시니어 프론트엔드 웹 개발자이며 주체적으로 개발 기술을 사용하며 재밌게 살고 있어요`,
     createdAt: `2017-07-07T00:00:00+09:00`,
-    updatedAt: `2021-03-18T00:00:00+09:00`,
+    updatedAt: `2023-03-27T14:00:00+09:00`,
 }
 
 const AboutPage: React.FC<PageProps> = () => {
@@ -20,6 +20,18 @@ const AboutPage: React.FC<PageProps> = () => {
 
     return (
         <PageLayout title={frontmatter.title} subtitle={frontmatter.subtitle}>
+            <p>
+                홍익대학교에서 산업디자인과를 졸업 후 만들고 싶은 웹사이트가 있어 독학으로 개발을 배웠어요.
+                좀 더 성장하기 위해 프리랜서 시장에 뛰어들었는데 의도하지 않게 10년 넘게 프론트엔드 웹 개발자로 일했지 뭐예요.
+                개발의 세계는 정말 끝이 없더라고요. 😇
+                도중에 운 좋게 만난 스타트업에서 일하면서 스타트업 문화와 스타트업에서 일하는 방식을 알차게 배웠어요.
+            </p>
+            <p>
+                🏝 지금은 프리랜서 생활을 정리하고 다시 나에게 집중하고 있는데,
+                퀀트 투자 및 기타 서비스 개발에 힘쓰며 평온하고도 작은 치열함 속에서 행복하게 하루하루를 보내고 있습니다.
+            </p>
+            <hr/>
+
             <h5>프리랜서 프론트엔드 웹 개발자</h5>
             <p>
                 2011년, 직군이 생소하던 그때부터 프리랜서로서 시작하여 현재까지 프론트엔드 웹 개발 분야에서 일하고 있습니다.
@@ -27,8 +39,8 @@ const AboutPage: React.FC<PageProps> = () => {
                 (검색엔진을 포함하여) 모두가 접근 가능한 웹사이트를 만들기 위해 노력하고 있습니다.
             </p>
             <ul>
-                {resumeData.skills.map(skill => (
-                    <li>{skill}</li>
+                {resumeData.skills.map((skill, index) => (
+                    <li key={`skill-${index}`}>{skill}</li>
                 ))}
             </ul>
             <hr/>
