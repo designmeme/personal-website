974f6eafca359a2539b4757d1cc2ec4c22945e86
jihye <ghe.lee19@gmail.com>
2023-04-10 12:52:43 +0900

    feat(ui): 포스트 목차 및 제목에 섹션 번호 붙이기

--- src/components/toc.tsx
+++ src/components/toc.tsx
@@ -22,14 +22,14 @@ const TocList: React.FC<Props> = ({toc, depth = 0}) => {
     return (
         <>
             {toc.items?.length && toc.items[0].url ? (
-                <ul>
+                <ol>
                     {toc.items?.map((item, index) => (
                         <li key={index}>
                             {item.url && <Link to={item.url}>{item.title}</Link>}
                             {item.items && <TocList toc={item} depth={depth}/>}
                         </li>
                     ))}
-                </ul>
+                </ol>
             ) :  toc.items?.map((item, index) => (
                 <TocList toc={item} depth={depth} key={`toc-child-${depth}-${index}`}/>
             ))}
