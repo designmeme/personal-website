a7d626f04de8ffa1026787cd6552b3bcd73200e3
Lee Jihye <ghe.lee19@gmail.com>
2026-08-12 18:53:06 +0900

    feat: update and expand content across profile, about, and resume pages

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,14 +5,15 @@ import SeoHead from "../../components/seo-head";
 import PageLayout, {PageFrontmatter} from "../../components/page-layout";
 import {useSiteMetadata} from "../../hooks/use-site-metadata";
 import resumeData from '../../data/resume.json'
+import CareerList from "../../components/career-list";
 
 
 export const frontmatter: PageFrontmatter = {
     title: `안녕하세요, 이지혜입니다`,
     subtitle: `👩‍🎨 👩‍💻 🚀 📈`,
-    excerpt: `시니어 프론트엔드 웹 개발자이며 주체적으로 개발 기술을 사용하며 재밌게 살고 있어요`,
+    excerpt: `시니어 프론트엔드 개발자이며 주체적으로 개발 기술을 사용하며 재밌게 살고 있어요`,
     createdAt: `2017-07-07T00:00:00+09:00`,
-    updatedAt: `2023-03-27T14:00:00+09:00`,
+    updatedAt: `2026-08-11T12:00:00+09:00`,
 }
 
 const AboutPage: React.FC<PageProps> = () => {
@@ -21,37 +22,46 @@ const AboutPage: React.FC<PageProps> = () => {
     return (
         <PageLayout frontmatter={frontmatter}>
             <p>
-                홍익대학교에서 산업디자인과를 졸업 후 만들고 싶은 웹사이트가 있어 독학으로 개발을 배웠어요.
-                좀 더 성장하기 위해 프리랜서 시장에 뛰어들었는데 의도하지 않게 10년 넘게 프론트엔드 웹 개발자로 일했지 뭐예요.
-                개발의 세계는 정말 끝이 없더라고요. 😇
-                도중에 운 좋게 만난 스타트업에서 일하면서 스타트업 문화와 스타트업에서 일하는 방식을 알차게 배웠어요.
-            </p>
-            <p>
-                🏝 지금은 프리랜서 생활을 정리하고 다시 나에게 집중하고 있는데,
-                퀀트 투자 및 기타 서비스 개발에 힘쓰며 평온하고도 작은 치열함 속에서 행복하게 하루하루를 보내고 있습니다.
+                홍익대학교에서 산업디자인과를 졸업 후 만들고 싶은 웹사이트가 있어 독학으로 개발을 배우기 시작했습니다.
+                좀 더 성장하기 위해 프리랜서 시장에 뛰어들어 15년 넘게 프론트엔드 개발자로 일했습니다.
+                저를 믿어준 분들 만나 스타트업에서 일하면서 스타트업 문화와 스타트업에서 일하는 방식도 알차게 배울 수 있었습니다.
             </p>
             <hr/>
 
             <h5>프리랜서 프론트엔드 개발자</h5>
-            <p>
-                2011년, 직군이 생소하던 그때부터 프리랜서로서 시작하여 현재까지 프론트엔드 웹 개발 분야에서 일하고 있습니다.
-                웹 표준에 맞춰 의미 있는 구조를 가지며, 다양한 환경에 반응하도록 디자인과 UX를 구현하고,
-                (검색엔진을 포함하여) 모두가 접근 가능한 웹사이트를 만들기 위해 노력하고 있습니다.
-            </p>
+
+            <p>React/Next.js, TypeScript, Angular 기반으로 아래 형태의 웹을 개발합니다.</p>
             <ul>
-                {resumeData.skills.map((skill, index) => (
-                    <li key={`skill-${index}`}>{skill}</li>
-                ))}
+                <li>
+                    <b>사용자 중심 웹서비스</b>
+                    <br/>
+                    신규 유입과 재방문을 높이는 사용성 중심 웹 개발에 중점을 둡니다.
+                    <br/>
+                    <div className="p-small" style={{marginTop: '-2px'}}>SEO / SSR / Analytics / UX / Lighthouse</div>
+                </li>
+                <li>
+                    <b>업무 효율성을 올리는 어드민</b>
+                    <br/>
+                    재사용 가능한 디자인 시스템을 바탕으로 업무에 필요한 다양한 UI를 개발하거나 외부 라이브러리를 사용합니다.
+                    <br/>
+                    <div className="p-small" style={{marginTop: '-2px'}}>Design system / Data Grid / Chart / Calendar UI / Drag and Drop / Shortcuts</div>
+                </li>
+                <li>
+                    <b>표현력이 중요한 서비스 소개 웹사이트</b>
+                    <br/>
+                    회사나 서비스 소개 사이트 등 가독성이 높으면서 브랜드를 잘 표현할 수 있게 시각 중심으로 표현합니다.
+                    <br/>
+                    <div className="p-small" style={{marginTop: '-2px'}}>Scroll Animation / Chart UI / Single Page Web</div>
+                </li>
             </ul>
-            <hr/>
-            <h5>같이 일해요</h5>
+
             <p>참여한 주요 작업 목록입니다.</p>
             <p className="p-small">헬로우봇 스킬스토어 / 헬로우봇 스튜디오 / 김메리 / 웨딩마루, 스튜디온, 웨딩북 웹 ERP / 이디움펀딩 / 성호그룹 / 엑스타 슈퍼챌린지 / LG Artcool / 현대자동차 제네시스 이러닝 / 투어팁스 하이브리드앱 / KB국민은행 희망별 / 현대카드뮤직 2.0 / 현대자동차 일마일</p>
             <p>
-                사용자를 고려한 디자인과 UX를 갖추면서 구조적이면서 빠르게 로딩되고 사용자가 어떤 기기로 보든지 기대한 대로 작동하는 웹사이트에 관심이 많습니다.
-                그런 프로젝트를 함께하고 싶어요. 저를 고용하는 데 관심이 간다면 <Link to="./resume">이력서 및 경력기술서</Link> 페이지를 봐주세요.
+                저를 고용하는 데 관심이 간다면 <Link to="./resume">이력서 및 경력기술서</Link> 페이지를 봐주세요.
             </p>
             <hr/>
+
             <h5>연락주세요</h5>
             <p>
                 저와 같이 일하고 싶거나 궁금한 것이 있나요?
--- src/pages/about/resume.tsx
+++ src/pages/about/resume.tsx
@@ -10,28 +10,61 @@ import {Link} from "gatsby";
 export const frontmatter: PageFrontmatter = {
     title: `이력서 및 경력기술서`,
     subtitle: `프론트엔드 개발자 이지혜`,
-    excerpt: `다양한 형태의 웹사이트와 모바일 웹, 반응형 웹 그리고 웹 접근성 프로젝트까지 풍부한 개발 경험을 가지고 있습니다.`,
+    excerpt: `15년 이상 경력의 프론트엔드 개발자로 다수의 프로젝트 개발 경험을 보유하고 있습니다.`,
     createdAt: `2017-07-07T00:00:00+09:00`,
-    updatedAt: `2023-03-31T13:00:00+09:00`,
+    updatedAt: `2026-08-11T12:00:00+09:00`,
 }
 
 const ResumePage: React.FC<PageProps> = () => {
     return (
         <PageLayout frontmatter={frontmatter}>
             <h3 style={{marginTop: '5rem'}}>이력서</h3>
-            <p>{frontmatter.excerpt}</p>
-
-            <hr/>
+            <p>{frontmatter.excerpt} 프리랜서로서 다양한 규모/기능/형태의 웹 서비스를 개발하고 있습니다.
+                스타트업에서 약 6년간 챗봇 빌더 및 관리 사이트, 웨딩 웹 솔루션 등 프론트엔드 개발 책임자로 일했습니다.
+            </p>
 
             <h5>프로필</h5>
             <dl className="profile-list">
                 <dt className="profile-title">이름</dt>
                 <dd className="profile-description">이지혜</dd>
-                <dt className="profile-title">이메일</dt>
+                <dt className="profile-title">연락처</dt>
                 <dd className="profile-description">ghe.lee19@gmail.com</dd>
-                <dt className="profile-title">홈페이지</dt>
-                <dd className="profile-description"><Link to={'/'}>https://heyjihye.netlify.app</Link></dd>
+                <dt className="profile-title">링크</dt>
+                <dd className="profile-description"><Link to={'https://www.linkedin.com/in/heyjihye'}>LinkedIn</Link> | <Link to={'https://github.com/designmeme'}>GitHub</Link></dd>
             </dl>
+
+            <hr/>
+
+            <h5>주요 강점</h5>
+            <ul>
+                <li>
+                    <b>높음 품질과 신뢰</b> 꼼꼼하게 요구 사항을 모두 구현하며 반복적인 테스트로 버그를 최소화한 품질 높은 산출물을 만듭니다.
+                    신뢰가 쌓여 연속해서 일을 받거나 외주로 일하던 스타트업에서 계약직으로 일하고 이어서 다른 스타트업으로 인연을 이어가기도 했습니다.
+                </li>
+                <li>
+                    <b>디자인과 기획 이해력</b> 산업디자인을 공부하여 디자인·기획에 대한 높은 이해도를 가지고 있으며,
+                    백엔드 영역과도 원활하게 협업하며 웹 제작 프로세스의 중간 다리 역할을 잘 수행합니다.
+                </li>
+                <li>
+                    <b>다양한 개발 경험</b> 신규 제작부터 주간 개선 사이클의 프로젝트, 다양한 규모의 회사, 다양한 UI와 기능, 1페이지부터 수십 페이지의 다양한 분량까지 폭 넓은 개발 경험을 갖고 있습니다.
+                </li>
+            </ul>
+
+            <hr/>
+
+            <h5>전문 기술</h5>
+            <p>전문 분야와 기술, 개발할 때 사용하는 도구입니다.</p>
+            <ul>
+                {resumeData.skills.map((skill, index) => (
+                    <li key={index.toString()}>{skill}</li>
+                ))}
+            </ul>
+            <p>협업을 위해 사용하는 도구입니다.</p>
+            <ul>
+                {resumeData.tools.map((tool, index) => (
+                    <li key={index.toString()}>{tool}</li>
+                ))}
+            </ul>
 
             <hr/>
 
@@ -45,37 +78,13 @@ const ResumePage: React.FC<PageProps> = () => {
 
             <hr/>
 
-            <h5>전문 기술</h5>
-            <p>전문 분야와 기술, 개발할 때 사용하는 도구입니다.</p>
+            {/*<hr className="print-hr"/>*/}
 
-            <ul>
-                {resumeData.skills.map((skill, index) => (
-                    <li key={index.toString()}>{skill}</li>
-                ))}
-            </ul>
-
-            <hr/>
-
-            <h5>협업</h5>
-            <p>
-                프로젝트 목표를 제대로 이해하고 기획 및 디자인을 존중하며 여러 직군과 협업합니다.
-                <br/>
-                소규모부터 대규모까지 다양한 규모의 프로젝트에서 팀원 및 분야 책임자로 일한 경험이 많습니다.
-            </p>
-
-            <ul>
-                {resumeData.tools.map((tool, index) => (
-                    <li key={index.toString()}>{tool}</li>
-                ))}
-            </ul>
-
-            <hr className="print-hr"/>
-
-            <h3>경력 기술서</h3>
-            <p>
-                웹 표준에 맞춰 의미 있는 구조를 가지며, 다양한 환경에 반응하도록 디자인과 UX를 구현하고,
-                (검색엔진을 포함하여) 모두가 접근 가능한 웹사이트를 만들기 위해 노력하고 있습니다.
-            </p>
+            {/*<h3>경력 기술서</h3>*/}
+            {/*<p>*/}
+            {/*    웹 표준에 맞춰 의미 있는 구조를 가지며, 다양한 환경에 반응하도록 디자인과 UX를 구현하고,*/}
+            {/*    (검색엔진을 포함하여) 모두가 접근 가능한 웹사이트를 만들기 위해 노력하고 있습니다.*/}
+            {/*</p>*/}
 
             <hr/>
 
@@ -92,7 +101,7 @@ const ResumePage: React.FC<PageProps> = () => {
 
             <h5>남김글</h5>
             <p>
-                능동적으로 프로젝트를 수행하고 다양한 기술과 협업 능력을 높이며 프론트엔드 웹 개발자가 지녀야 할 자질을 쌓아왔습니다.
+                능동적으로 프로젝트를 수행하고 다양한 기술과 협업 능력을 높이며 프론트엔드 개발자가 지녀야 할 자질을 쌓아왔습니다.
                 <br/>
                 항상 새로운 기술에 대한 관심과 배우고자 하는 열망을 바탕으로 업무에 임하겠습니다.
             </p>
